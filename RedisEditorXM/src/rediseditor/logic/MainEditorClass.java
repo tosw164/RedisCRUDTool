c5889a9d9c3c040d5aa470f676fc9c81de764913
tosw164 <tosw164@auckland.ac.nz>
2016-12-08 10:31:35 +1300

    Added Multithreading for refreshing Jtable contents
    + Removed unused imports
    + Deleted sorter field for the table since not used outside creation
    + Initialised scrollpane so program doens't throw nullpointer exception
    + Moved creation of JTable logic into multithreaded method
      (Therefore refreshTable() is used whenever table needs population/refresh)
    + Deleted old setupKeyValueList() method since functionality now in swingworker

--- RedisEditorXM/src/rediseditor/logic/MainEditorClass.java
+++ RedisEditorXM/src/rediseditor/logic/MainEditorClass.java
@@ -24,9 +24,6 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
-import org.omg.PortableServer.THREAD_POLICY_ID;
-
-import redis.clients.jedis.exceptions.JedisNoReachableClusterNodeException;
 import rediseditor.gui.DialogBoxes;
 import rediseditor.gui.UiInitialise;
 import rediseditor.redis.RedisController;
@@ -45,8 +42,6 @@ public class MainEditorClass extends JPanel {
 	private JTable table;
 	private DefaultTableModel table_model;
 	private JScrollPane scroll_pane;
-	private TableRowSorter<TableModel> sorter;
-
 
 	public static void main(String[] args) {		
 		EventQueue.invokeLater(new Runnable() {
@@ -74,7 +69,8 @@ public class MainEditorClass extends JPanel {
 		controller = RedisController.getInstance(""); //TODO delete later don't always want localhost
 
 		initialise_button_components();
-
+		scroll_pane = new JScrollPane(new JTable()); //Initialise scrollpane so remove doesn't throw NPE
+		
 		//Initialise container for button cluster on the left
 		Box left_button_cluster = Box.createVerticalBox();
 
@@ -96,8 +92,7 @@ public class MainEditorClass extends JPanel {
 
 		setLayout(new BorderLayout());
 		add(left_button_cluster, BorderLayout.WEST);
-		add(setupKeyValueList(), BorderLayout.CENTER);
-		this.remove(new JButton());
+		refreshTable();
 	}
 
 	private void initialise_button_components(){
@@ -208,65 +203,76 @@ public class MainEditorClass extends JPanel {
 		System.exit(0);
 	}
 
-	private JScrollPane setupKeyValueList(){
-		String[] table_headings = {"Key", "Value"};
-		int row_count = 0;
-
-		table_model = new DefaultTableModel(table_headings, row_count){
-			public boolean isCellEditable(int row, int column){
-				return true;
-			}
-
-			public Class getColumnClass(int column){
-				switch (column) {
-				default:
-					return String.class;
-				}
-			}
-		};
-
-		table = new JTable(table_model);
-
-		for (String[] s: controller.getKeyValuePairData()){
-			((DefaultTableModel) table_model).addRow(s);
-		}
-
-		table.setModel(table_model);
-		table.getColumnModel().getColumn(0).setPreferredWidth(300);
-		table.getColumnModel().getColumn(1).setPreferredWidth(900);
-
-
-		//For ordering
-		sorter = new TableRowSorter<TableModel>(table_model);
-		table.setRowSorter(sorter);
-		ArrayList<RowSorter.SortKey> key = new ArrayList<RowSorter.SortKey>();
-		key.add(new RowSorter.SortKey(0, SortOrder.ASCENDING));//default is ascending order of first column i.e. alphabetical order of words
-		sorter.setSortKeys(key);
-		sorter.sort();
-
-		table.getTableHeader().setReorderingAllowed(false);
-
-		//Alignment for the cells http://stackoverflow.com/a/7433758
-		DefaultTableCellRenderer alignment_renderer = new DefaultTableCellRenderer();
-		alignment_renderer.setHorizontalAlignment(JLabel.CENTER);
-		table.setDefaultRenderer(String.class, alignment_renderer);
-		table.setDefaultRenderer(Integer.class, alignment_renderer);
-
-		//adds scroll pane to table to panel
-		scroll_pane = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
-		scroll_pane.setVisible(true);
-		scroll_pane.setMinimumSize(new Dimension(550, 400));
-
-		return scroll_pane;
-	}
-
 	//TODO multithread this
 	private void refreshTable(){
 		this.remove(scroll_pane);
-		add(setupKeyValueList(), BorderLayout.CENTER);
-		frame.pack();
+		
+		new SwingWorker<Void, Object[]>(){
+
+			@Override
+			protected Void doInBackground() throws Exception {
+				String[] table_headings = {"Key", "Value"};
+				int row_count = 0;
+
+				TableModel worker_model = new DefaultTableModel(table_headings, row_count){
+					public boolean isCellEditable(int row, int column){
+						return true;
+					}
+
+					public Class getColumnClass(int column){
+						switch (column) {
+						default:
+							return String.class;
+						}
+					}
+				};
+
+				JTable worker_table = new JTable(worker_model);
+
+				for (String[] s: controller.getKeyValuePairData()){
+					((DefaultTableModel) worker_model).addRow(s);
+				}
+
+				worker_table.setModel(worker_model);
+				worker_table.getColumnModel().getColumn(0).setPreferredWidth(300);
+				worker_table.getColumnModel().getColumn(1).setPreferredWidth(900);
+
+
+				//For ordering
+				TableRowSorter<TableModel> worker_sorter = new TableRowSorter<TableModel>(worker_model);
+				worker_table.setRowSorter(worker_sorter);
+				ArrayList<RowSorter.SortKey> key = new ArrayList<RowSorter.SortKey>();
+				key.add(new RowSorter.SortKey(0, SortOrder.DESCENDING));//default is ascending order of first column i.e. alphabetical order of words
+				worker_sorter.setSortKeys(key);
+				worker_sorter.sort();
+
+				worker_table.getTableHeader().setReorderingAllowed(false);
+
+				//Alignment for the cells http://stackoverflow.com/a/7433758
+				DefaultTableCellRenderer alignment_renderer = new DefaultTableCellRenderer();
+				alignment_renderer.setHorizontalAlignment(JLabel.CENTER);
+				worker_table.setDefaultRenderer(String.class, alignment_renderer);
+				worker_table.setDefaultRenderer(Integer.class, alignment_renderer);
+
+				//adds scroll pane to table to panel
+				JScrollPane worker_scrollpane = new JScrollPane(worker_table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+				worker_table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+				worker_scrollpane.setVisible(true);
+				worker_scrollpane.setMinimumSize(new Dimension(550, 400));
+				
+				Object[] component_package = new Object[]{worker_table, worker_model, worker_scrollpane};
+				publish(component_package);
+				return null;
+			}
+
+			@Override
+			protected void process(List<Object[]> chunks) {
+				table = (JTable) chunks.get(chunks.size() -1)[0];
+				table_model = (DefaultTableModel) chunks.get(chunks.size() - 1)[1];
+				scroll_pane = (JScrollPane) chunks.get(chunks.size() - 1)[2];
+				add(scroll_pane, BorderLayout.CENTER);
+				frame.pack();
+			}
+		}.execute();
 	}
-
-
 }
